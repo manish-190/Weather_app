461f5bd2e1b9b9d4775e7b0e777c12a84ef192f0
manish <nessfi000@gmail.com>
2024-11-01 12:57:36 +0530

    work under

---
+++ src/components/Weather.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Weather.css'  //working 
+//import Weather from './weather.css' //not working
+
+
+const Weather = () => {
+  return (
+    <div>
+      <h1>Weather</h1>
+    </div>
+  )
+}
+
+export default Weather
